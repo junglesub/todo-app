d3df839e30218ab76533fec47aa4733c40719de8
junglesub <junglesubmarine@gmail.com>
2022-01-18 21:38:59 +0900

    finished

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# todo-app
-연습용 Todo 사이트
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [newTodoName, setNewTodoName] = useState("");
+  const [allTodoItems, setAllTodoItems] = useState([]);
+
+  const removeItem = (entry) => {
+    setAllTodoItems((state) => state.filter((item) => item !== entry));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="create">
+        <h1>Todo App</h1>
+        <input
+          id="todo-item-input"
+          value={newTodoName}
+          onChange={(event) => setNewTodoName(event.target.value)}
+        />
+        <button
+          id="create-btn"
+          onClick={() => {
+            setAllTodoItems((state) => [...state, newTodoName]);
+            setNewTodoName("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Create
+        </button>
+      </div>
+      <div className="list" id="todo-list">
+        {allTodoItems.map((item) => (
+          <div key={item} onClick={() => removeItem(item)}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
